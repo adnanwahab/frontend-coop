d4cc49efd2b6beeed326058e269584e9a4e05181
_ <93497294+shelbernstein@users.noreply.github.com>
2023-08-08 11:46:05 -0500

    coop polling party online

--- hello-world/hello-world.js
+++ hello-world/hello-world.js
@@ -1,50 +1,104 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const Sheets = require('node-sheets').default;
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
 
-const handler = async (event) => {
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'creds.json');
 
-
-  callApi
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-    body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
-
-module.exports = { handler }
-
-
-async function callApi() {
-  try {
-    const gs = new Sheets('1amfst1WVcQDntGe6walYt-4O5SCrHBD5WntbjhvfIm4');
-    const authData = require('./client_secret_137652990869-7fpfbjnlsidolcmkftic4i7n643eggrs.apps.googleusercontent.com.json'); // authData = { client_email, private_key }
-    await gs.authorizeJWT(authData);
-    const table = await gs.tables('Formats!A1:E3');
-    console.log(table.headers);
-    console.log(table.formats);
-    console.log(table.rows);
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
   } catch (err) {
-    console.error(err);
+    return null;
   }
 }
 
-callApi()
+/**
+ * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+async function listMajors(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  const res = await sheets.spreadsheets.values.get({
+    //spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+    spreadsheetId: '1J5Aj4v8Vte0W6nnYvoWgyG448MD5N07n_WFyocQtKsM',
+    range: 'Sheet1!A1:B2',
+  });
+  const rows = res.data.values;
+  if (!rows || rows.length === 0) {
+    console.log('No data found.');
+    return;
+  }
+  console.log('Name, Major:');
+  rows.forEach((row) => {
+    // Print columns A and E, which correspond to indices 0 and 4.
+    console.log(`${row[0]}, ${row[1]}`);
+  });
+
+  let value = parseFloat(rows[0][0])
+
+  let response = await sheets.spreadsheets.values.update({
+    spreadsheetId: '1J5Aj4v8Vte0W6nnYvoWgyG448MD5N07n_WFyocQtKsM',
+    range: 'Sheet1!A1',
+    valueInputOption: 'RAW',
+    requestBody: {
+        values: [[value + 2]]  // 2D array
+    }
+});
+
+}
+
+authorize().then(listMajors).catch(console.error);
